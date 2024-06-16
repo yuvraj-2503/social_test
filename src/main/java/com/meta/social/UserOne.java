c0c13387676b13cb975f8031fb7318eefee51fe4
yuvraj-2503 <singh.yuvraj1047@gmail.com>
2024-06-16 18:50:19 +0530

    social client flow tested

--- src/main/java/com/meta/social/UserOne.java
+++ src/main/java/com/meta/social/UserOne.java
@@ -4,9 +4,7 @@ import com.meta.client.auth.core.AuthManager;
 import com.meta.client.auth.core.AuthManagerImpl;
 import com.meta.client.core.SocialManager;
 import com.meta.client.core.SocialManagerImpl;
-import com.meta.common.pojos.LoginUserDto;
-import com.meta.common.pojos.RegisterUserDto;
-import com.meta.common.pojos.UserResponse;
+import com.meta.common.pojos.*;
 import com.meta.common.users.PhoneNumber;
 import com.meta.common.users.UserDetails;
 import com.meta.common.users.UserStatus;
@@ -15,24 +13,30 @@ public class UserOne {
     private static AuthManager authManager;
     private static SocialManager socialManager;
     private static UserResponse user;
-    private static final String SERVER_URL = "http://localhost:8080/api/v1";
+    private static Config config;
+
+    private static final String jsonConfig = "{\n" +
+            "\"userServerBaseUrl\"      :       \"http://localhost:8080/api/v1\",\n" +
+            "\"baseUrl\"                :       \"http://localhost:8081/api/v1\"\n" +
+            "}";
+
+    private static void authenticate() {
+        authManager = new AuthManagerImpl(config.getUserServerBaseUrl());
+        LoginUserDto userDto = new LoginUserDto();
+        userDto.setEmailId("singh.yuvraj1047@gmail.com");
+        userDto.setPassword("Pass@123");
+        user = authManager.signIn(userDto).join();
+    }
 
     public static void main(String[] args) {
+        config = ConfigHandler.getFromJson(jsonConfig);
         authenticate();
-//        socialManager = new SocialManagerImpl(SERVER_URL);
-//        registerUser();
+        socialManager = new SocialManagerImpl(config, user);
 
         System.out.println(user.getUserId());
         System.out.println(user.getToken());
-    }
 
-    private static void authenticate() {
-        authManager = new AuthManagerImpl(SERVER_URL);
-        LoginUserDto userDto = new LoginUserDto();
-        userDto.setEmailId("singh.yuvraj1047@gmail.com");
-//        userDto.setFullName("Yuvraj Singh");
-        userDto.setPassword("Pass@123");
-        user = authManager.signIn(userDto).join();
+        registerUser();
     }
 
     private static void registerUser() {
